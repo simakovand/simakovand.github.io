42449724e7962064bd219cc3e9f3ad8c7973db57
Sima <simakovand9@gmail.com>
2024-02-13 22:12:22 +0300

    clear

--- src/redux/rootReducer.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import first from './slices/first.ts'
-
-export const rootReducer = combineReducers({
-	first
-})
-export type RootState = ReturnType<typeof rootReducer>
